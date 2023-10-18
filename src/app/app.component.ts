c4f56a9da11800572c6f74e6f5b6e9e01707d21a
carlos170800 <carlos.collado101@gmail.com>
2023-10-18 16:23:39 -0700

    upd

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,7 +2,7 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-root',
-  templateUrl: './app.component.html',
+  templateUrl: './app.component.html', //Aqui esta mandadno a llamar todo lo que hay en el HTML PRINCIPAL MAIN  
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
